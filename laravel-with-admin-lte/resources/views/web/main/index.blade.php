99a7f58cc8b3a0cb87d10ca589effe3002bd1c2f
DevGentle <developer.taned@gmail.com>
2016-09-12 23:29:19 +0700

    Update code na ja

---
+++ laravel-with-admin-lte/resources/views/web/main/index.blade.php
@@ -0,0 +1,10 @@
+@extends('web.layout')
+
+@section('content')
+    <div >
+        <img src="{{ asset('img/resource/header_01.jpg') }}">
+    </div>
+    <div>
+        <img src="{{ asset('img/resource/product_header_01.jpg') }}">
+    </div>
+@endsection
