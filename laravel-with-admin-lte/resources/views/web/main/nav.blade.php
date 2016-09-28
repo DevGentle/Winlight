9b006b855cf0bc1526c6c0512ef22bcc5af47043
DevGentle <developer.taned@gmail.com>
2016-09-29 02:01:46 +0700

    Hot fixed

--- laravel-with-admin-lte/resources/views/web/main/nav.blade.php
+++ laravel-with-admin-lte/resources/views/web/main/nav.blade.php
@@ -1,37 +1,19 @@
 <div class="container-fluid p-r-l-0">
-    {{--<div class="navbar-header">--}}
-        <div class="navbar-header__menu">
-            <div class="col-xs-2 col-xs-offset-2 navbar-header__menu-image"><img src="{{ asset('img/resource/winlight_logo_white.png') }}"></div>
-            <div class="col-xs-6 text-center navbar-header__menu-box">
-                <ul>
-                    <li><a href="{{ url('/') }}">วินเนอร์</a></li>
-                    <li><a href="{{ url('/product') }}">ผลิตภัณฑ์</a></li>
-                    <li><a href="{{ url('/service') }}">บริการ</a></li>
-                    <li><a href="{{ url('/reference') }}">ผลงาน</a></li>
-                    <li><a href="">ติดต่อสอบถาม</a></li>
-                </ul>
-            </div>
+    <div class="navbar-header__menu">
+        <div class="col-xs-2 col-xs-offset-2 navbar-header__menu-image"><img src="{{ asset('img/resource/winlight_logo_white.png') }}"></div>
+        <div class="col-xs-6 text-center navbar-header__menu-box">
+            <ul>
+                <li><a href="{{ url('/') }}">วินเนอร์</a></li>
+                <li><a href="{{ url('/product') }}">ผลิตภัณฑ์</a></li>
+                <li><a href="{{ url('/service') }}">บริการ</a></li>
+                <li><a href="{{ url('/reference') }}">ผลงาน</a></li>
+                <li><a href="">ติดต่อสอบถาม</a></li>
+            </ul>
         </div>
-        <div class="navbar-header__cover">
-            <img data-u="image" src="{{ asset('img/resource/slider_01.jpg') }}" />
-            <img data-u="image" src="{{ asset('img/resource/slider_02.jpg') }}" />
-        </div>
-        <div class="navbar-header__green-line"></div>
-        {{--<div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 1300px; height: 350px; overflow: hidden; visibility: hidden;">--}}
-            {{--<div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 0px; width: 1300px; height: 350px; overflow: hidden;">--}}
-                {{--<div data-p="225.00" style="display: none;">--}}
-                    {{--<img data-u="image" src="{{ asset('img/resource/slider_01.jpg') }}" />--}}
-                {{--</div>--}}
-                {{--<div data-p="225.00" style="display: none;">--}}
-                    {{--<img data-u="image" src="{{ asset('img/resource/slider_02.jpg') }}" />--}}
-                {{--</div>--}}
-            {{--</div>--}}
-            {{--<div data-u="navigator" class="jssorb05" style="bottom:16px;right:16px;" data-autocenter="1">--}}
-                {{--<!-- bullet navigator item prototype -->--}}
-                {{--<div data-u="prototype" style="width:16px;height:16px;"></div>--}}
-            {{--</div>--}}
-            {{--<span data-u="arrowleft" class="jssora22l" style="top:0px;left:12px;width:40px;height:58px;" data-autocenter="2"></span>--}}
-            {{--<span data-u="arrowright" class="jssora22r" style="top:0px;right:12px;width:40px;height:58px;" data-autocenter="2"></span>--}}
-        {{--</div>--}}
-    {{--</div>--}}
+    </div>
+    <div class="navbar-header__cover">
+        <img data-u="image" src="{{ asset('img/resource/slider_01.jpg') }}" />
+        <img data-u="image" src="{{ asset('img/resource/slider_02.jpg') }}" />
+    </div>
+    <div class="navbar-header__green-line"></div>
 </div>
