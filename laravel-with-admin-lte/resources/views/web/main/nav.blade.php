681d97698eb6734cac31a1b618b546e2f72d9ef1
DevGentle <developer.taned@gmail.com>
2016-10-01 00:48:12 +0700

    Hot fixed nav nar

--- laravel-with-admin-lte/resources/views/web/main/nav.blade.php
+++ laravel-with-admin-lte/resources/views/web/main/nav.blade.php
@@ -1,15 +1,17 @@
 <div class="container-fluid p-r-l-0">
-    <div class="container">
-        <div class="navbar-header__menu">
-            <div class="col-xs-1 navbar-header__menu-image"><img src="{{ asset('img/resource/winlight_logo_white.png') }}"></div>
-            <div class="col-xs-9 text-center navbar-header__menu-box">
-                <ul>
-                    <li><a href="{{ url('/') }}">วินเนอร์</a></li>
-                    <li><a href="{{ url('/product') }}">ผลิตภัณฑ์</a></li>
-                    <li><a href="{{ url('/service') }}">บริการ</a></li>
-                    <li><a href="{{ url('/reference') }}">ผลงาน</a></li>
-                    <li><a href="">ติดต่อสอบถาม</a></li>
-                </ul>
+    <div class="row">
+        <div class="container">
+            <div class="navbar-header__menu">
+                <div class="col-xs-1 navbar-header__menu-image"><img src="{{ asset('img/resource/winlight_logo_white.png') }}"></div>
+                <div class="col-xs-9 text-center navbar-header__menu-box">
+                    <ul>
+                        <li><a href="{{ url('/') }}">วินเนอร์</a></li>
+                        <li><a href="{{ url('/product') }}">ผลิตภัณฑ์</a></li>
+                        <li><a href="{{ url('/service') }}">บริการ</a></li>
+                        <li><a href="{{ url('/reference') }}">ผลงาน</a></li>
+                        <li><a href="">ติดต่อสอบถาม</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
