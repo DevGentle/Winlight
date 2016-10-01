9d849f456e92eeeefaf88922a22e2e6f85297870
DevGentle <developer.taned@gmail.com>
2016-10-02 01:36:21 +0700

    Add product hover && rebase reference page for hover

--- laravel-with-admin-lte/resources/views/web/reference/reference.blade.php
+++ laravel-with-admin-lte/resources/views/web/reference/reference.blade.php
@@ -21,49 +21,34 @@
         <div class="text-center">
             <div class="col-md-10 col-md-offset-1 reference__content">
                 <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('img/resource/reference/ref_08.jpg') }}">
+                    <div class="reference__content--item">
+                        <img src="{{ asset('img/resource/reference/ref_08.jpg') }}">
+                    </div>
                 </div>
                 <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('img/resource/reference/ref_09.jpg') }}">
+                    <div class="reference__content--item">
+                        <img src="{{ asset('img/resource/reference/ref_09.jpg') }}">
+                    </div>
                 </div>
                 <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('img/resource/reference/ref_11.jpg') }}">
+                    <div class="reference__content--item">
+                        <img src="{{ asset('img/resource/reference/ref_11.jpg') }}">
+                    </div>
                 </div>
                 <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('img/resource/reference/ref_16.jpg') }}">
+                    <div class="reference__content--item">
+                        <img src="{{ asset('img/resource/reference/ref_16.jpg') }}">
+                    </div>
                 </div>
                 <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('img/resource/reference/ref_17.jpg') }}">
+                    <div class="reference__content--item">
+                        <img src="{{ asset('img/resource/reference/ref_17.jpg') }}">
+                    </div>
                 </div>
                 <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('img/resource/reference/ref_19.jpg') }}">
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('img/resource/reference/ref_25.jpg') }}">
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('img/resource/reference/ref_26.jpg') }}">
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('img/resource/reference/ref_28.jpg') }}">
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('img/resource/reference/ref_34.jpg') }}">
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('img/resource/reference/ref_35.jpg') }}">
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('img/resource/reference/ref_37.jpg') }}">
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('img/resource/reference/ref_43.jpg') }}">
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('img/resource/reference/ref_44.jpg') }}">
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <img src="{{ asset('img/resource/reference/ref_46.jpg') }}">
+                    <div class="reference__content--item">
+                        <img src="{{ asset('img/resource/reference/ref_19.jpg') }}">
+                    </div>
                 </div>
             </div>
         </div>
