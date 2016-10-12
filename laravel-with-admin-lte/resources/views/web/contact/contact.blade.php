0024b470d53a5ebda0dae565679b4ffc0ead2db7
DevGentle <developer.taned@gmail.com>
2016-10-12 22:32:20 +0700

    Add contact form

--- laravel-with-admin-lte/resources/views/web/contact/contact.blade.php
+++ laravel-with-admin-lte/resources/views/web/contact/contact.blade.php
@@ -16,7 +16,28 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-xs-4 contact-index__content-form">Form</div>
+                    <div class="col-xs-4 contact-index__content-form">
+                        <form>
+                            <div class="form-group">
+                                <label>ชื่อผู้มาติดต่อ</label>
+                                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="ชื่อผู้มาติดต่อ">
+                            </div>
+                            <div class="form-group">
+                                <label>อีเมลล์</label>
+                                <input type="email" class="form-control" id="exampleInputPassword1" placeholder="อีเมลล์">
+                            </div>
+                            <div class="form-group">
+                                <label>เบอร์โทรศัพท์</label>
+                                <input type="tel" class="form-control" id="exampleInputPassword1" placeholder="เบอร์โทรศัพท์">
+                            </div>
+                            <div class="form-group">
+                                <label>ข้อความ</label>
+                                <textarea class="form-control" rows="11"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-success">ยืนยัน</button>
+                            <button type="reset" class="btn btn-default">ยกเลิก</button>
+                        </form>
+                    </div>
                     <div class="col-xs-8 contact-index__content-map">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.6524872835043!2d100.63182244621967!3d13.739477172844818!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311d61cfdd2dd24f%3A0x5f8744cb2dbccd3!2s7+%E0%B8%8B%E0%B8%AD%E0%B8%A2+4+Khwaeng+Suan+Luang%2C+Khet+Suan+Luang%2C+Krung+Thep+Maha+Nakhon+10250!5e0!3m2!1sen!2sth!4v1476281447217" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
                     </div>
